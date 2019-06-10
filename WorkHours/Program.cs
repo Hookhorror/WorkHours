4ed524762512a51ebcec0e309b69800e6d237b8e
Hookhorror <joni.kiviluoto@gmail.com>
2019-06-10 20:32:48 +0300

    First commit

---
+++ WorkHours/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Work Hours!");
+        }
+    }
+}
